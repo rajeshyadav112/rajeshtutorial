bf5bb284c5615bbdbc04b417423d7b9e71edc02f
hp <hp@LAPTOP-EGGV59UT>
2025-02-01 10:34:04 +0530

    3rd commit new browser launch is added to gtihub class

--- Weekend930maven/src/test/java/testng/GithubCommitCheck.java
+++ Weekend930maven/src/test/java/testng/GithubCommitCheck.java
@@ -1,9 +1,19 @@
 package testng;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class GithubCommitCheck {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("www.google.com");
+		System.out.println(driver.getTitle());
+		
 
 	}
 
